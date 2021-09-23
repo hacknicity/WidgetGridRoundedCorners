7891adda8d40aa87b06656d7abea7847fbf42cd9
Geoff Hackworth <geoff@hacknicity.com>
2021-09-23 15:01:28 +0100

    Initial Commit

---
+++ WidgetGridRoundedCorners/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WidgetGridRoundedCorners
+//
+//  Created by Geoff Hackworth on 23/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WidgetGridRoundedCorners/WidgetGridRoundedCornersApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetGridRoundedCornersApp.swift
+//  WidgetGridRoundedCorners
+//
+//  Created by Geoff Hackworth on 23/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct WidgetGridRoundedCornersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
